f5d109a48b8283f69a4bbc586b4cb4e3299e46a4
elifboluk <101395811+elifboluk@users.noreply.github.com>
2022-04-19 11:48:39 +0300

    .

--- AdminTemplate/Program.cs
+++ AdminTemplate/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages()
+    .AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
