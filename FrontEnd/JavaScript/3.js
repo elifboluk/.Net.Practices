0cc114c3a7684eec704fc276297ae653367843d0
elifboluk <101395811+elifboluk@users.noreply.github.com>
2022-04-27 12:26:46 +0300

    .

--- FrontEnd/JavaScript/3.js
+++ FrontEnd/JavaScript/3.js
@@ -7,12 +7,18 @@ var changeColor=() => {
     var pickerDiv=document.getElementById("picker-div");
 
     console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
- //  var color="rgb(" + rangeRed.value + "," + rangeBlue.value + ")"; -- alt satırda ki kod ile aynı
+ // var color="rgb(" + rangeRed.value + "," + rangeBlue.value + ")"; -- alt satırda ki kod ile aynı
     var color = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`;
     var colorRev = `rgb(${255 - rangeRed.value},${255 - rangeGreen.value},${255 - rangeBlue.value})`;
     pickerDiv.innerHTML=color;
     pickerDiv.style.backgroundColor=color;
     pickerDiv.style.color=colorRev;
+}
+var copyClipboard =() =>{ //arrow function
+    var pickerDiv=document.getElementById("picker-div");
+    navigator.clipboard.writeText(pickerDiv.innerHTML);
+    alert("Kopyalandı: " + pickerDiv.innerHTML);
+
 }
 
 changeColor();
