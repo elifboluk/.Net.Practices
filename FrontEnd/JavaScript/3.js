af3e0bcb316310cdf8823d11023ef6b08d5209c3
elifboluk <101395811+elifboluk@users.noreply.github.com>
2022-04-27 14:43:39 +0300

    animate css

--- FrontEnd/JavaScript/3.js
+++ FrontEnd/JavaScript/3.js
@@ -1,39 +1,50 @@
-console.log("3.js");
-var changeColor=() => {
-    var rangeRed=document.getElementById("range-red");
-    var rangeGreen=document.getElementById("range-green");
-    var rangeBlue=document.getElementById("range-blue");
+var changeColor = () => {
+    var rangeRed = document.getElementById("range-red");
+    var rangeGreen = document.getElementById("range-green");
+    var rangeBlue = document.getElementById("range-blue");
+    var pickerDiv = document.getElementById("picker-div");
 
-    var pickerDiv=document.getElementById("picker-div");
-
-    console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
- // var color="rgb(" + rangeRed.value + "," + rangeBlue.value + ")"; -- alt satırda ki kod ile aynı
+    //console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
+    //var color = "rgb(" + rangeRed.value + "," + rangeGreen.value + "," + rangeBlue.value + ")";
     var color = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`;
     var colorRev = `rgb(${255 - rangeRed.value},${255 - rangeGreen.value},${255 - rangeBlue.value})`;
-    pickerDiv.innerHTML=color;
-    pickerDiv.style.backgroundColor=color;
-    pickerDiv.style.color=colorRev;
+    pickerDiv.innerHTML = color;
+    pickerDiv.style.backgroundColor = color;
+    pickerDiv.style.color = colorRev;
 }
-var copyClipboard =() =>{ //arrow function
-    ;
+var copyClipboard = () => { //arrow function
+
     Swal.fire({
         icon: 'question',
-        title: 'Kopyalansın mı?',        
+        title: 'Kopyalansın mı?',
         showCancelButton: true,
         confirmButtonText: 'Kopyala',
-        denyButtonText: `İptal`,
-      }).then((result) => {
+        cancelButtonText: `İptal`,
+    }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
-        //console.log(result)
+        //console.log(result);
         if (result.isConfirmed) {
-          var pickerDiv=document.getElementById("picker-div");
-          navigator.clipboard.writeText(pickerDiv.innerHTML, "succes")
-          Swal.fire('Kopyalandı!', pickerDiv.innerHTML, 'success')
+            var pickerDiv = document.getElementById("picker-div");
+            navigator.clipboard.writeText(pickerDiv.innerHTML);
+            Swal.fire('Kopyalandı!', pickerDiv.innerHTML, 'success')
         } else if (result.isDenied) {
-          Swal.fire('Kopyalama işlemi iptal edildi', '', 'info')
+            Swal.fire('İptal', 'Kopyalama işlemi iptal edildi', 'info')
         }
-      })
+    })
+}
 
+var divMouseOver = () => { 
+    var pickerDiv = document.getElementById("picker-div");
+    
+    pickerDiv.classList.add("animate__animated");
+    pickerDiv.classList.add("animate__pulse");
+}
+
+var divMouseLeave = () => { 
+    var pickerDiv = document.getElementById("picker-div");
+    
+    pickerDiv.classList.remove("animate__animated");
+    pickerDiv.classList.remove("animate__pulse");
 }
 
 changeColor();
