0f658e1b05ee5b22b82d658fda5f6651df11fec4
elifboluk <101395811+elifboluk@users.noreply.github.com>
2022-05-06 11:02:41 +0300

    category api1

---
+++ AdminTemplate/Controllers/Apis/CategoryApiController.cs
@@ -0,0 +1,41 @@
+﻿using AdminTemplate.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers.Apis
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryApiController : ControllerBase
+    {
+        private readonly MyContext _context;
+
+        public CategoryApiController(MyContext context)
+        {
+            _context = context;
+        }
+        // CRUD - Create Read Update Delete
+        // [HttpGet] Read (SELECT)
+        // [HttpPost] Create (INSERT)
+        // [HttpPut] Update (UPDATE)
+        // [HttpDelete] Delete (DELETE)
+        // Get isteklerinde nasıl bir data çekeceğimizi query string ile belirleriz.
+        // Get ile data gönderemez. header bilgisi gönderebilir.
+        // Post, put ve delete işlemlerinde data gönderebiliriz. header-body bilgisi gönderilebilir.
+        // Header = Query String
+
+        [HttpGet]
+        public IActionResult All()
+        {
+            try
+            {
+                return Ok(_context.Categories.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = $"Bir hata oluştu: {ex.Message}" });
+            }
+        }
+
+    }
+}
